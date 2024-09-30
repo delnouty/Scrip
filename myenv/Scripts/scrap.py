d1959ef2a3cdf43656fe696a1536f694b94de1d8
Darya Filatova <daria_filatova@interia.pl>
2024-09-30 11:47:45 +0200

    Hello

--- myenv/Scripts/scrap.py
+++ myenv/Scripts/scrap.py
@@ -3,7 +3,7 @@ from bs4 import BeautifulSoup
 
 URL = "https://realpython.github.io/fake-jobs/"
 page = requests.get(URL)
-
+##
 soup = BeautifulSoup(page.content, "html.parser")
 
 results = soup.find(id="ResultsContainer")
